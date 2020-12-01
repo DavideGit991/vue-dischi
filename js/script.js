503b41d1add6d0d4a9c182c79de29a2349d05f61
DavideGit991 <pasto.davide1991@gmail.com>
2020-12-01 15:01:12 +0100

    ultimo push

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Descrizione:
+// Facciamo una chiamata ajax all’api di boolean al seguente indirizzo.
+// https://flynn.boolean.careers/exercises/api/array/music
+// L’api ci restituirà una decina di dischi musicali che dovremo stampare a schermo con Vue.
+
+var app = new Vue({
+  el:"#app",
+  data:{
+    playList:[]
+  },
+
+  mounted:function () {
+    axios
+    .get("https://flynn.boolean.careers/exercises/api/array/music")
+    .then((risposta) =>{
+      this.playList = risposta.data.response
+      console.log(this.playList);
+    })
+
+  },
+
+
+
+
+
+
+
+});
