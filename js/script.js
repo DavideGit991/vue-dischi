d9fa71321d957c0a2030fc108338125244477528
DavideGit991 <pasto.davide1991@gmail.com>
2020-12-01 16:00:02 +0100

    miglioramenti layout finale

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
 // https://flynn.boolean.careers/exercises/api/array/music
 // L’api ci restituirà una decina di dischi musicali che dovremo stampare a schermo con Vue.
 
+// Bonus:
+// Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo solo i corrispondenti cd.
+
 var app = new Vue({
   el:"#app",
   data:{
@@ -14,7 +18,9 @@ var app = new Vue({
     .get("https://flynn.boolean.careers/exercises/api/array/music")
     .then((risposta) =>{
       this.playList = risposta.data.response
+      console.log(risposta.data.response);
       console.log(this.playList);
+
     })
 
   },
